5706abda563537468ff723cc8821934766c44a3b
Анастасия <a.fevraleva@maii.ru>
2021-08-23 22:43:19 +0200

    collective additional income information

--- venv/Scripts/GUIBacker.py
+++ venv/Scripts/GUIBacker.py
@@ -2,6 +2,11 @@ from tkinter import*
 from tkinter import messagebox
 from tkinter.ttk import Combobox
 
+def click():
+            if additional_income_availability_entry.get() == "да" or additional_income_availability_entry.get()=="ecть":
+
+
+
 
 
 # create window
@@ -51,14 +56,14 @@ credit_size_label.grid(column=0,row=3)
 
 # credit size entry
 
-creadt_size_text=StringVar()
+creadt_size_text=IntVar()
 credit_size_entry=Entry(textvariable=creadt_size_text)
 credit_size_entry.grid(column=1,row=3)
 
-credit_tern_label=Label(window,text="Размер кредита",font=("Arial",25))
+credit_tern_label=Label(window,text="Срок кредита",font=("Arial",25))
 credit_tern_label.grid(column=0,row=4)
 
-creadt_size_text=StringVar()
+creadt_size_text=IntVar()
 credit_size_entry=Entry(textvariable=creadt_size_text)
 credit_size_entry.grid(column=1,row=4)
 
@@ -66,7 +71,7 @@ credit_size_entry.grid(column=1,row=4)
 
 # Create salary size label
 
-salary_size_label=Label(window,text="размер зарплаты",font=("Arial",25))
+salary_size_label=Label(window,text="Размер зарплаты",font=("Arial",25))
 salary_size_label.grid(column=0,row=5)
 
 
@@ -74,6 +79,20 @@ salary_size_label.grid(column=0,row=5)
 salary_size=Combobox()
 salary_size['value']=(15000,20000,30000,35000,40000,45000,50000,55000,60000,65000,70000,750000,80000)
 salary_size.grid(column=1,row=5)
+
+additional_income_availability_label=Label(window,text="Наличие дополнительного дохода")
+additional_income_availability_label.grid(column=0,row=6)
+
+additional_income_availability=StringVar()
+additional_income_availability_entry=Entry(textvariable=additional_income_availability)
+additional_income_availability_entry.grid(column=1,row=6)
+
+additional_income_size_label=Label(window,text="Размер дополнительного дохода,руб")
+additional_income_size_label.grid(column=0,row=7)
+
+additional_income_size=Combobox()
+additional_income_size['values']=(1000,5000,10000,15000,20000,25000,30000,350000,40000)
+additional_income_size.grid(column=1,row=7)
 
 
 
